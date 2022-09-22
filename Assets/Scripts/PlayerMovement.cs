3fe8377552fb4b39b448a442de6c6f1c94f9afa2
Sergio <sergioloc2@gmail.com>
2022-09-22 13:48:29 +0200

    Calculate gravity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,26 @@ public class PlayerMovement : MonoBehaviour {
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     public float turnSmoothVelocity = 0.1f;
+
+    [Header("Gravity")]
+    public float gravity;
+    private float currentGravity;
+    public float constantGravity;
+    public float maxGravity;
+    private Vector3 gravityMovement;
     
     void Start() {
         controller = GetComponent<CharacterController>();
     }
 
     void Update() {
+        CalculateGravity();
+        Movement();
+    }
+
+    #region Movement
+
+    private void Movement() {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(h, 0f, v).normalized;
@@ -24,9 +38,30 @@ public class PlayerMovement : MonoBehaviour {
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Vector3 moveDir = (Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward) + gravityMovement;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
+
+    #endregion
+
+    #region Gravity
+
+    private bool IsGrounded() {
+        return controller.isGrounded;
+    }
+
+    private void CalculateGravity() {
+        if (IsGrounded()) {
+            currentGravity = constantGravity;
+        }
+        else if (currentGravity > maxGravity) {
+                currentGravity -= gravity * Time.deltaTime;
+        }
+
+        gravityMovement = Vector3.down * -currentGravity;
+    }
+
+    #endregion
 
 }
